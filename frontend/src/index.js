7d7d9330892dca76f4e6c1f8f0681a28060f80c6
Luiz Souza <looizao@looizao.com>
2023-08-09 09:30:39 -0300

    project starting point

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import store from './store'
 import './index.css';
 import './bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
 
